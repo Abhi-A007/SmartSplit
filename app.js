3baf02aba96d30faa25ebbb82d76575cfcc163db
Abhi-A007 <abhirupray.somu@gmail.com>
2024-12-02 15:54:40 +0530

    fixing

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("signup.ejs");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
